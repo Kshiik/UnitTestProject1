a8c7ab7bc5f4c448b122e38c0e57382b79897d87
New <New@KOMPUTER>
2023-09-19 01:58:12 +0300

    Добавьте файлы проекта.

---
+++ StringCheckLibrary/StringCheck.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace StringCheckLibrary
+{
+    public class StringCheck
+    {
+        /// <summary>
+        /// Проверка stringName на наличие символов: русские буквы, пробел и дефис
+        /// Строка stringName не должна превышать 50 сиволов
+        /// </summary>
+        /// <param name="stringName"></param>
+        /// <returns></returns>
+        public bool CheckName(string stringName)
+        {
+            string regex = @"^((а-я)|(\s)|(\-))+$";
+            string regexWhiteSpace = @"^((\s)|(\-))+$";
+            if (Regex.Match(stringName, regexWhiteSpace, RegexOptions.IgnoreCase).Success)
+            {
+                return false;
+            }
+            else
+            {
+                if (Regex.Match(stringName, regex, RegexOptions.IgnoreCase).Success)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+    }
+}
---
+++ UnitTestProject1/StringCheckLibraryTests.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using StringCheckLibrary;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class StringCheckLibraryTests
+    {
+        /// <summary>
+        /// Тестирование на пустую строку
+        /// </summary>
+        [TestMethod]
+        public void CheckName_isEmpty_falseReturned()
+        {
+            //Arrange
+            string stringName="";
+            //Act
+            StringCheck obj = new SrtingCheck();
+            bool result = obj.CheckName(StringName);
+            //Assert
+            Assert.IsFalse(result);
+        }
+        /// <summary>
+        /// Тестирование на корректное имя
+        /// </summary>
+        [TestMethod]
+        public void CheckName_isCorectName_trueReturned()
+        {
+            //Arrange
+            string stringName = "Важенина-ина Ульяна";
+            //Act
+            StringCheck obj = new SrtingCheck();
+            bool result = obj.CheckName(StringName);
+            //Assert
+            Assert.IsTrue(result);
+        }
+        /// <summary>
+        /// Тестирование на некорректное имя(латинские буквы)
+        /// </summary>
+        [TestMethod]
+        public void CheckName_isLatinеLetters_falseReturned()
+        {
+            //Arrange
+            string stringName = "Vazhenina Ulyana";
+            //Act
+            StringCheck obj = new SrtingCheck();
+            bool result = obj.CheckName(StringName);
+            //Assert
+            Assert.IsFalse(result);
+        }
+        /// <summary>
+        /// Тестирование на некорректное имя(спец символы)
+        /// </summary>
+        [TestMethod]
+        public void CheckName_isSpecialSymbol_falseReturned()
+        {
+            //Arrange
+            string stringName = "Важенина123 Ульяна#";
+            //Act
+            StringCheck obj = new SrtingCheck();
+            bool result = obj.CheckName(StringName);
+            //Assert
+            Assert.IsFalse(result);
+        }
+        /// <summary>
+        /// Тестирование на некорректное имя(только тире)
+        /// </summary>
+        [TestMethod]
+        public void CheckName_isTire_falseReturned()
+        {
+            //Arrange
+            string stringName = " ----";
+            //Act
+            StringCheck obj = new SrtingCheck();
+            bool result = obj.CheckName(StringName);
+            //Assert
+            Assert.IsFalse(result);
+        }
+        /// <summary>
+        /// Тестирование на некорректное имя(больше 50 символов)
+        /// </summary>
+        [TestMethod]
+        public void CheckName_isMoreThan50_falseReturned()
+        {
+            //Arrange
+            string stringName = "Важенина Ульянаааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааподвжлапьлваитловаыподулкптва";
+            //Act
+            StringCheck obj = new SrtingCheck();
+            bool result = obj.CheckName(StringName);
+            //Assert
+            Assert.IsFalse(result);
+        }
+
+    }
+}
